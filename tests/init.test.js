ccfd3c51caf9492b3d958e828260d35b18f8cfe7
Daniel Leite de Oliveira <dleitee@gmail.com>
2017-02-21 18:53:15 -0300

    fix tests

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,15 +1,15 @@
 import { Money } from '../src'
 
 test('should be returned a Money object', () => {
-  expect(Money.init(100).toString()).toBe('100.00')
+  expect(Money.init(100).getValue()).toBe('100.00')
 })
 
 test('should be returned a Money object with value = 0', () => {
-  expect(Money.init().toString()).toBe('0.00')
+  expect(Money.init().getValue()).toBe('0.00')
 })
 
 test('should be returned a Money object with value = 100 and locale pt-BR', () => {
-  expect(Money.init(1, { locale: 'pt-BR' }).toString()).toBe('1,00')
+  expect(Money.init(1, { locale: 'pt-BR' }).getValue()).toBe('1.00')
 })
 
 test('should be returned an exception when value is a NaN', () => {
